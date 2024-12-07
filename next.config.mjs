657d626f32cd67b905de619d0a4aa93be497a831
Hieu <hieun201104@gmail.com>
2024-12-07 08:12:00 -0800

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
-/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  images: {
+    unoptimized: true // Disable default image optimization
+  },
+  assetPrefix: isProd ? "/2dterrain/" : "",
+  basePath: isProd ? "/2dterrain" : "",
+  output: "export"
 };
 
 export default nextConfig;
