e38c88ac0c0b7c3ec98d7a11b0d59b88e407dae1
Hieu <hieun201104@gmail.com>
2024-12-07 08:06:34 -0800

    Updates

---
+++ _next/static/ehSddhH2p_krB-cj99Tbh/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/379-6ff482f3d9b3aa68.js","static/css/c27d3780468847f1.css","static/chunks/pages/index-02292c3e81afa62b.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
